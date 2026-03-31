efd1dcf6131e260a1b273a263bf6c2bc1d2d62fc
zerodayhunter375 <arr1781945@gmail.com>
2026-03-31 16:37:54 +0900

    fix: regex replace http to https in auth_url

--- apps/users/github_backend.py
+++ apps/users/github_backend.py
@@ -1,16 +1,24 @@
+import os
 from social_core.backends.github import GithubOAuth2
-
-CALLBACK = 'https://black-message-production.up.railway.app/oauth/complete/github/'
 
 class GithubOAuth2HTTPS(GithubOAuth2):
     REDIRECT_STATE = False
 
     def get_redirect_uri(self, state=None):
-        return CALLBACK
+        return os.environ.get(
+            'SOCIAL_AUTH_GITHUB_CALLBACK_URL',
+            'https://black-message-production.up.railway.app/oauth/complete/github/'
+        )
+
+    def auth_complete_url(self):
+        return self.get_redirect_uri()
 
     def auth_url(self):
         url = super().auth_url()
-        return url.replace(
-            'http://black-message-production.up.railway.app',
-            'https://black-message-production.up.railway.app'
+        import re
+        url = re.sub(
+            r'redirect_uri=http%3A',
+            'redirect_uri=https%3A',
+            url
         )
+        return url
